7198a846ce57337a0d2470da077cb01103bafa5e
Wang Xuerui <idontknw.wang@gmail.com>
2014-09-12 15:39:05 +0800

    BoardConfig.mk: initial work

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,24 @@
-USE_CAMERA_STUB := true
+#
+# Copyright 2013 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# inherit from the proprietary version
+# Use the non-open-source parts, if they're present
 -include vendor/lenovo/a808t/BoardConfigVendor.mk
+
+
+USE_CAMERA_STUB := true
 
 TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
@@ -15,15 +32,20 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 
 TARGET_BOOTLOADER_BOARD_NAME := a808t
 
-BOARD_KERNEL_CMDLINE := 
+BOARD_KERNEL_CMDLINE := console=ttyMT3,921600n1 vmalloc=496M slub_max_order=0 lcm=0-nt35521_hd720_dsi_vdo_boe fps=6163 bootprof.pl_t=696 bootprof.lk_t=1208 printk.disable_uart=1
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 
 # fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+TARGET_USERIMAGES_USE_EXT4 := true
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00a00000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00a00000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1577058304  # 0x5e000000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1237319680  # 0x49c00000
+BOARD_CACHEIMAGE_PARTITION_SIZE := 134217728  # 0x08000000
+BOARD_CACHEIMAGE_TYPE := ext4
+
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_PREBUILT_KERNEL := device/lenovo/a808t/kernel

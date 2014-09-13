41c096f6a44635db26d1665bf486b6f0a646b635
Wang Xuerui <idontknw.wang@gmail.com>
2014-09-14 00:05:11 +0800

    some trivial fixes

--- cm.mk
+++ cm.mk
@@ -4,6 +4,10 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Release name
 PRODUCT_RELEASE_NAME := a808t
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
@@ -13,6 +17,6 @@ $(call inherit-product, device/lenovo/a808t/device_a808t.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a808t
 PRODUCT_NAME := cm_a808t
-PRODUCT_BRAND := lenovo
-PRODUCT_MODEL := a808t
-PRODUCT_MANUFACTURER := lenovo
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := A808t
+PRODUCT_MANUFACTURER := Lenovo

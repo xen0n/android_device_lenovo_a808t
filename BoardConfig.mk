d9a09ecaaa99da72767850898392142cb470a609
Wang Xuerui <idontknw.wang@gmail.com>
2014-09-14 15:06:20 +0800

    BoardConfig.mk: adjust cpu variant

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,7 +29,7 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_VARIANT := cortex-a9
 
 ARCH_ARM_HAVE_TLS_REGISTER := true
 

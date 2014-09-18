3fac5ee4b7dc053122a09e54a5460a3790c132e0
Wang Xuerui <idontknw.wang@gmail.com>
2014-09-18 09:43:25 +0800

    root/makestpnodes.sh: append instead of replacing

--- root/makestpnodes.sh
+++ root/makestpnodes.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
 
 mknod /dev/stpwmt c 190 0 > /dev/makestp.log 2>&1
-mknod /dev/stpgps c 191 0 > /dev/makestp.log 2>&1
-mknod /dev/stpbt  c 192 0 > /dev/makestp.log 2>&1
+mknod /dev/stpgps c 191 0 >> /dev/makestp.log 2>&1
+mknod /dev/stpbt  c 192 0 >> /dev/makestp.log 2>&1
